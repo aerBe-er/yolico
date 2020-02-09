51e44ee56f45f509ac908ee8464c50886857b8e2
yolico <1823234122@qq.com>
2020-02-09 19:29:54 +0800

    添加返回顶部功能

--- js/index.js
+++ js/index.js
@@ -3,22 +3,30 @@ $(function () {
     $(window).scroll(function () {
         if ($(document).scrollTop() > 0) {
             $('.header').addClass('header2');
-           
+            $('.top').slideDown(500);
+
         } else {
             $('.header').removeClass('header2');
+            $('.top').slideUp(500);
         }
+    })
+    $('.top').click(function () {
+        $('html,body').stop().animate({
+            scrollTop: 0
+        })
     })
     // 搜索区域
     // console.log($('.search input'));
-    $('.search input').focus(function() {
+    $('.search input').focus(function () {
         $(this).removeClass('add')
     })
-    $('.search input').blur(function() {
+    $('.search input').blur(function () {
         $(this).addClass('add')
-    
+
     })
     // 攻略按钮
-    $('#strategy').click(function() {
-        location.href='null.html';
+    $('#strategy').click(function () {
+        location.href = 'null.html';
     })
+
 })
